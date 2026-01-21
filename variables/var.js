52b6d85a79c5b839df9af555f53b0d50b1f1e91a
unknown <navinavitha2504@gmail.com>
2026-01-21 18:15:39 +0530

    variables completed

---
+++ variables/var.js
@@ -0,0 +1,16 @@
+/*
+var name
+var email = null
+var phone = "9988"
+console.log(name);
+console.log(email);
+console.log(phone);
+*/
+
+//we can create varibles by using let , var , const 
+var name = "navi"
+let email = "navitha2gmail.com"
+const gender = "female"
+console.log(name);
+console.log(email);
+console.log(gender);
