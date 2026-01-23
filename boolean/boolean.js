fc466a8b3fc592ec6f7ef75829d28b57fa8e70bb
unknown <navinavitha2504@gmail.com>
2026-01-23 09:39:33 +0530

    my practice

---
+++ boolean/boolean.js
@@ -0,0 +1,26 @@
+/* boolean data type 
+let a = true 
+let b = false 
+console.log(a);
+console.log(b);  */
+
+// comparision operators
+/*let a = 56
+let b = 45
+console.log(a==b); //true
+console.log(a===b);//false
+console.log(a!=b);//false
+console.log(a!==b);//true  i.e string != number
+console.log(a>b);//true
+console.log(a<b);//false
+console.log(a>=b);//true 
+console.log(a<=b);//false */
+
+//logical operatos 
+console.log(true && true );
+console.log(true|| false );
+console.log(!true);
+let a = 34
+let b = 23
+let c = 98
+console.log(a>b && a>c); //true and false = false
---
+++ conditional/conditional.js
@@ -0,0 +1,65 @@
+/*let a = 45
+let b = 56
+if(a>b){
+    console.log("iam navi");
+}
+console.log("iam venu ");*/
+
+/*let c = 6
+let d = 7
+let e = 17
+if(c<d && d<e){
+    console.log("e is bigger than c & d");   
+}
+else if (c<d){
+    console.log("c is small number");    
+}
+else {
+    console.log("d is small number");   
+}
+console.log("iam confused"); */
+
+/*
+let a = 0
+if(a){
+    console.log("it is a number");
+}
+else {
+    console.log("it is not a number ");
+    
+}
+
+//switch 
+let option = a
+switch (option) {
+    case a :
+        console.log("a option is selected");
+        break;
+    case a :
+        console.log("a option is selected");
+        break;
+    case b :
+        console.log("b option is selected");
+        break;
+    case c :
+        console.log("c option is selected");
+        break;        
+     default:
+        console.log("no option is selected");
+     break;
+} */
+
+//ternary operator (?)
+/*
+let n = 56
+let p = 45
+console.log(n<p ? "p is big" : "n is big");
+
+let k = 36
+let l = 90
+let c = k > l ? k : l 
+console.log(c); */
+
+//nullish coalescing
+let a = 'navitha'
+console.log(a ?? "no value"); // if it has undefined or null no value will print 
