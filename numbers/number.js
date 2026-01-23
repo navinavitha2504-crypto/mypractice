e44a45615b16d0c11e24277104da2b10e1295069
unknown <navinavitha2504@gmail.com>
2026-01-23 09:34:58 +0530

    my practice

---
+++ numbers/number.js
@@ -0,0 +1,48 @@
+let a = 45
+console.log(a);
+console.log(typeof a);
+let b = 34e-3 // exponential no i.e 34000
+console.log(b);
+//floating points 
+let c = (0.1 * 10) + (0.2 * 10 )/10
+console.log(c);
+
+//arthematic operators
+let d = 16
+let e = 4
+let f = d%e
+console.log(f);
+//++ and -- operators
+let num = 16
+console.log(num++); // post increment
+//console.log(++num)  pre increment 
+console.log(num);
+
+let h = 2**2  // this is for power 
+console.log(h);
+
+//assignment operators 
+let i = +2  // i.e a = a+2
+console.log(i);
+
+let j = 34 
+j += 4;   // i.e 34+4
+console.log(j);
+
+//big int data type 
+let k = 1323232323232323
+let l = 9999999999999999n;
+console.log(k);
+console.log(l);
+console.log(typeof k);
+console.log(typeof l);
+
+// string concatination using operators 
+let m = "navi"
+let n = 7
+let o = m -n // only using + it will concatinate 
+console.log(o);
+
+
+
+
