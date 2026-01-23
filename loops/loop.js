3665a4768c5f2fd87ab6b729d4c16bf1f735c2da
unknown <navinavitha2504@gmail.com>
2026-01-23 09:41:53 +0530

    my practice

---
+++ loops/loop.js
@@ -0,0 +1,60 @@
+/*//for loop 
+// numbers from 1 to 100
+for(let i = 1; i<=10; i++){
+    console.log(i);
+}
+//to display decrement nos
+for(let i =10;i>=1;i--){
+    console.log(i);
+    
+}  */
+
+/*//while loop 
+//nos from 1 to 10 
+let i = 1
+while (i<=10) {
+    console.log(i);
+    i++
+}
+
+//nos from reverse order
+let j=10
+while(j>=1){
+    console.log(j);
+    j--
+    
+}*/
+
+/*//do while 
+let k= 4
+do{
+    console.log("hi");
+    console.log("iam from do while");   
+}while (k>=1) */
+    
+/*//check the pin no 
+let pin = "0000" 
+do{
+    console.log("give  current pin number"+pin);
+    pin++
+}while (pin!=5)
+console.log("you entered correct pin "+pin);
+console.log("welcome");  */
+
+//break keyword used in loops(for loop)
+ for(let i = 0; i <=10;i++){
+    break        //it will execute but it is not the crt way 
+    console.log(i);
+    console.log('hello');  
+ }
+
+ let persons=10
+ for(let i=1;i<persons;i++){
+    console.log(i);
+    if(i==5){
+        break
+    }
+ }
+
+ //break in while loop
+ 
